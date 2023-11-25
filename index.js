ad6e9a038167e80f5d71fa07ae6fa56c4e5a77a6
Matt Batman <batman.matt@gmail.com>
2023-11-25 14:51:23 -0500

    Make an output folder if not exists

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+import fs from "fs";
 import TurndownService from "turndown";
-import data from "./data.json" assert { type: 'json' };
+import data from "./data.json" assert { type: "json" };
 
-console.log(data);
+const HTML_KEY = "quote";
+const OUTPUT_DIR = "./dist";
+
+function main() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  const turndownService = new TurndownService();
+
+  data.forEach(function (d) {
+    const htmlString = d[HTML_KEY];
+    const markdown = turndownService.turndown(htmlString);
+  });
+}
+
+main();
