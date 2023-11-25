d9bda763bcc14b2914260c68c1de60ca8ce68651
Matt Batman <batman.matt@gmail.com>
2023-11-25 15:18:07 -0500

    Write markdown files and content

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import TurndownService from "turndown";
 import data from "./data.json" assert { type: "json" };
 
-const HTML_KEY = "quote";
 const OUTPUT_DIR = "./dist";
 
 function main() {
@@ -12,10 +11,34 @@ function main() {
 
   const turndownService = new TurndownService();
 
-  data.forEach(function (d) {
-    const htmlString = d[HTML_KEY];
+  data.forEach(function ({ id, quotee, quote, platform, link }) {
+    const htmlString = quote;
     const markdown = turndownService.turndown(htmlString);
+    const file = getFileName({ id, quotee });
+    const fileData = getFileData({ markdown, id, quotee, platform, link });
+
+    fs.writeFile(file, fileData, function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
   });
+}
+
+function getFileName({ id, quotee }) {
+  return `${OUTPUT_DIR}/${id}-${quotee}.md`;
+}
+
+function getFileData({ markdown, id, quotee, platform, link }) {
+  return `---
+id: ${id}
+quotee: "${quotee}"
+platform: "${platform}"
+link: "${link}"
+---
+
+${markdown}
+`;
 }
 
 main();
